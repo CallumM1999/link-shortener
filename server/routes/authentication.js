569ec3f70d7e8f52933eef504c599497002d1280
cal <callummac@protonmail.com>
2019-02-06 15:28:07 +0000

    Validated user register/login data

--- server/routes/authentication.js
+++ server/routes/authentication.js
@@ -5,13 +5,16 @@ const userLogin = require('../db/methods/userLogin');
 const userRegister = require('../db/methods/userRegister');
 
 const bcyrpt = require('bcrypt');
+const validator = require('validator');
 
 router.post('/login', async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) return res.status(400).send();
 
+    const normalizedEmail = validator.normalizeEmail(email);
+
     try {
-        const queryResponse = await userLogin(email);
+        const queryResponse = await userLogin(normalizedEmail);
 
         // email not found
         if (!queryResponse.length) return res.status(401).send('user not found');
@@ -37,10 +40,25 @@ router.post('/register', async (req, res) => {
     const { email, password } = req.body;
     if (!email || !password) return res.status(400).send();
 
+    // dont help user with error codes, if server side validation is required, they are bypassing client validation
+    const trimmedEmail = validator.trim(email);
+    if (!validator.isEmail(trimmedEmail)) return res.status(401).send();
+
+    const normalizedEmail = validator.normalizeEmail(trimmedEmail);
+
+    // password requirements
+    // =====================
+    // length 8 - 100
+    // ascii characters
+
+    const trimmedPassword = validator.trim(password);
+    const match = trimmedPassword.match(/(?=^(.{8,100})$)(?=(.*[0-9].*){2})/);
+    if (!match) return res.status(401).send();
+
     const passwordHash = await bcyrpt.hash(password, 10);
 
     try {
-        const queryResponse = await userRegister(email, passwordHash);
+        const queryResponse = await userRegister(normalizedEmail, passwordHash);
 
         req.login(queryResponse.insertId, err => {
             if (err) console.log('error', err);
