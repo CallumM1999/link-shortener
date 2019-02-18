e6c99259f4a1f5051de96f296459afaf9900fad6
cal <callummac@protonmail.com>
2019-02-18 12:55:40 +0000

    Added link to header

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -2,7 +2,9 @@ export default Vue.component('page-header', {
     template: `
         <header class="header">
             <div class="container">
-                <h1 class="title">Link Shortener</h1>
+                <a href='/'>
+                    <h1 class="title">Link Shortener</h1>
+                </a>
             </div>
         </header>
     `
